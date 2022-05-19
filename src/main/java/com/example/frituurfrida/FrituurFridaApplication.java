d001e79c1095f62f7eb00a90f0f392b1ad8f639b
Aewyn <Aewyn@outlook.com>
2022-05-19 10:57:20 +0200

    First commit.

---
+++ src/main/java/com/example/frituurfrida/FrituurFridaApplication.java
@@ -0,0 +1,13 @@
+package com.example.frituurfrida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrituurFridaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrituurFridaApplication.class, args);
+    }
+
+}
