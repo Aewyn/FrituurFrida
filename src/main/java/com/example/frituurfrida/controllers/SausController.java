91443b23fea6bc3866621e4f858846c37706fa68
Aewyn <aewyn@outlook.com>
2022-06-02 11:02:10 +0200

    Added images for the website as well as RequestMapping functionality.

--- src/main/java/com/example/frituurfrida/controllers/SausController.java
+++ src/main/java/com/example/frituurfrida/controllers/SausController.java
@@ -2,13 +2,19 @@ package com.example.frituurfrida.controllers;
 
 import com.example.frituurfrida.domain.Saus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
+@RequestMapping("sauzen")
 public class SausController {
     private final Saus[] alleSauzen = {
             new Saus(1,"cocktail", new String[] {"mayonaise", "ketchup", "whisky"}),
@@ -18,8 +24,21 @@ public class SausController {
             new Saus(5,"vinaigrette", new String[] {"wijnazijn", "mosterd", "olie"})
     };
 
-    @GetMapping("/sauzen")
+    @GetMapping
     private ModelAndView findAll(){
-        return new ModelAndView("sauzen", "alleSauzen", alleSauzen);
+        return new ModelAndView("sauzen",
+                "alleSauzen",
+                alleSauzen);
+    }
+
+    private Optional<Saus> findByIdHelper(long id){
+        return Arrays.stream(alleSauzen).filter(saus -> saus.getNummer()==id).findFirst();
+    }
+
+    @GetMapping("{id}")
+    public ModelAndView findById(@PathVariable long id){
+        var modelAndView = new ModelAndView("saus");
+        findByIdHelper(id).ifPresent(gevondenSaus->modelAndView.addObject("saus", gevondenSaus));
+        return modelAndView;
     }
 }
