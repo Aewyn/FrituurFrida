a1ed2a1016985962a60c2e30079141a4e653e9f4
Aewyn <aewyn@outlook.com>
2022-06-08 11:53:44 +0200

    Added alfabet page and changed SausController to work with it

--- src/main/java/com/example/frituurfrida/controllers/SausController.java
+++ src/main/java/com/example/frituurfrida/controllers/SausController.java
@@ -9,33 +9,49 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 @Controller
 @RequestMapping("sauzen")
 public class SausController {
     private final Saus[] alleSauzen = {
-            new Saus(1,"cocktail", new String[] {"mayonaise", "ketchup", "whisky"}),
-            new Saus(2,"mayonaise", new String[] {"olie", "eidooiers", "mosterd", "azijn"}),
-            new Saus(3,"mosterd", new String[] {"mosterdzaad", "water", "zout", "azijn"}),
-            new Saus(4,"tartare", new String[] {"eitjes", "augurk", "kappertjes", "mayonaise", "verse kruiden"}),
-            new Saus(5,"vinaigrette", new String[] {"wijnazijn", "mosterd", "olie"})
+            new Saus(1, "cocktail", new String[]{"mayonaise", "ketchup", "whisky"}),
+            new Saus(2, "mayonaise", new String[]{"olie", "eidooiers", "mosterd", "azijn"}),
+            new Saus(3, "mosterd", new String[]{"mosterdzaad", "water", "zout", "azijn"}),
+            new Saus(4, "tartare", new String[]{"eitjes", "augurk", "kappertjes", "mayonaise", "verse kruiden"}),
+            new Saus(5, "vinaigrette", new String[]{"wijnazijn", "mosterd", "olie"})
     };
 
     @GetMapping
-    private ModelAndView findAll(){
-        return new ModelAndView("sauzen",
-                "alleSauzen",
-                alleSauzen);
+    private ModelAndView findAll() {
+        return new ModelAndView("sauzen", "alleSauzen", alleSauzen);
     }
 
-    private Optional<Saus> findByIdHelper(long id){
-        return Arrays.stream(alleSauzen).filter(saus -> saus.getNummer()==id).findFirst();
+    private Optional<Saus> findByIdHelper(long id) {
+        return Arrays.stream(alleSauzen).filter(saus -> saus.getNummer() == id).findFirst();
     }
 
     @GetMapping("{id}")
-    public ModelAndView findById(@PathVariable long id){
+    public ModelAndView findById(@PathVariable long id) {
         var modelAndView = new ModelAndView("saus");
-        findByIdHelper(id).ifPresent(gevondenSaus->modelAndView.addObject("saus", gevondenSaus));
+        findByIdHelper(id).ifPresent(gevondenSaus -> modelAndView.addObject("saus", gevondenSaus));
         return modelAndView;
+    }
+
+    private final char[] alfabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+
+    @GetMapping("alfabet")
+    public ModelAndView alfabet() {
+        return new ModelAndView("sausAlfabet", "alfabet", alfabet);
+    }
+
+    private Stream<Saus> findByBeginNaamHelper(char letter) {
+        return Arrays.stream(alleSauzen).filter(saus -> saus.getNaam().charAt(0) == letter);
+    }
+
+    @GetMapping("alfabet/{letter}")
+    public ModelAndView findByBeginNaam(@PathVariable char letter) {
+        return new ModelAndView("sausAlfabet", "alfabet", alfabet)
+                .addObject("sauzen", findByBeginNaamHelper(letter).iterator());
     }
 }
