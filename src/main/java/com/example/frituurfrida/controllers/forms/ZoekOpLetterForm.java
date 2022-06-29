80e295b0e1320dba520876b71443499ea4575f54
Aewyn <aewyn@outlook.com>
2022-06-29 10:09:39 +0200

    Added bean validation to project.

--- src/main/java/com/example/frituurfrida/controllers/forms/ZoekOpLetterForm.java
+++ src/main/java/com/example/frituurfrida/controllers/forms/ZoekOpLetterForm.java
@@ -2,8 +2,6 @@ package com.example.frituurfrida.controllers.forms;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Positive;
-import javax.validation.constraints.Size;
 
 public record ZoekOpLetterForm (@NotNull @Pattern(regexp =  "[a-zA-Z]") String beginLetter){
 }
